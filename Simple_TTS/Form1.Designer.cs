2b8596c52cc09b0b7e5f2cf5547dfcebca5b983b
Md.Shaon Khalifa <shaonkhalifa@gmail.com>
2023-05-01 23:37:03 +0600

    Initial Commit

---
+++ Simple_TTS/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Simple_TTS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextHolder = new System.Windows.Forms.RichTextBox();
+            this.TTS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextHolder
+            // 
+            this.TextHolder.Location = new System.Drawing.Point(33, 54);
+            this.TextHolder.Name = "TextHolder";
+            this.TextHolder.Size = new System.Drawing.Size(466, 251);
+            this.TextHolder.TabIndex = 0;
+            this.TextHolder.Text = "";
+            this.TextHolder.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextHolder_KeyDown);
+          
+            // 
+            // TTS
+            // 
+            this.TTS.Location = new System.Drawing.Point(610, 148);
+            this.TTS.Name = "TTS";
+            this.TTS.Size = new System.Drawing.Size(107, 49);
+            this.TTS.TabIndex = 1;
+            this.TTS.Text = "TTS";
+            this.TTS.UseVisualStyleBackColor = true;
+            this.TTS.Click += new System.EventHandler(this.TTS_Click);
+        
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TTS);
+            this.Controls.Add(this.TextHolder);
+            this.Name = "TTS";
+            this.Text = "Simple TTS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox TextHolder;
+        private System.Windows.Forms.Button TTS;
+    }
+}
+
---
+++ Simple_TTS/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Simple_TTS
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TTS_Click(object sender, EventArgs e)
+        {
+            string Text = TTS.Text;
+            synthesizer.Speak(Text);
+            synthesizer.Speak(TextHolder.Text );
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            synthesizer.SetOutputToDefaultAudioDevice();
+        }
+        private void TextHolder_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (!e.SuppressKeyPress)
+            {
+                string text = "";
+
+                if (e.KeyCode == Keys.Enter)
+                {
+                    text = "Enter";
+                }
+                else if (e.KeyCode == Keys.Back)
+                {
+                    text = "Backspace";
+                }
+                else if (e.KeyCode == Keys.Space)
+                {
+                    text = "Space";
+                }
+                else if (e.Shift && e.KeyCode != Keys.ShiftKey)
+                {
+                    text = "Shift " + e.KeyCode.ToString();
+                }
+                else if (e.Alt)
+                {
+                    text =   e.KeyCode.ToString();
+                }
+                else if (e.Control)
+                {
+                    text =  e.KeyCode.ToString();
+                }
+                else if (e.KeyCode == Keys.CapsLock)
+                {
+                    text = "Caps Lock " + (Control.IsKeyLocked(Keys.CapsLock) ? "On" : "Off");
+                }
+                else if (e.KeyCode != Keys.ShiftKey && e.KeyCode != Keys.ControlKey && e.KeyCode != Keys.Menu)
+                {
+                    text = e.KeyCode.ToString();
+                }
+
+                if (text != "")
+                {
+                    synthesizer.Speak(text);
+                }
+            }
+        }
+    }
+}
